844ff25fa80db6aeae31ea5232dd770777b0a022
NectDz <54990613+NectDz@users.noreply.github.com>
2024-09-21 10:47:47 -0400

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import * as React from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./pages/Home/Home";
+import Navbar from "./Components/navbar";
 
 function App() {
   return (
     <ChakraProvider>
+      <Navbar />
       <Home />
     </ChakraProvider>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { ChakraProvider, Box, Flex, Link, Stack, Text } from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <ChakraProvider>
+      <Box position="fixed" top="0" width="100%" zIndex="100" bg="transparent">
+        <Flex as="nav" justify="left" py={4} px={8} backdropFilter="blur(10px)">
+          <Stack direction="row" spacing={8} align="center">
+            <Link
+              href="#about"
+              _hover={{ textDecoration: "none", color: "teal.500" }}
+            >
+              <Text fontSize="lg" fontWeight="bold">
+                About
+              </Text>
+            </Link>
+            <Link
+              href="#contact"
+              _hover={{ textDecoration: "none", color: "teal.500" }}
+            >
+              <Text fontSize="lg" fontWeight="bold">
+                Contact
+              </Text>
+            </Link>
+          </Stack>
+        </Flex>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default Navbar;
