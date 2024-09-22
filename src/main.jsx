9e91564d5b19e11fa38755cdd45bd81df5522471
NectDz <54990613+NectDz@users.noreply.github.com>
2024-09-22 12:44:13 -0400

    Fixed Firebase Auth

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,20 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Team from "./pages/Team/Team";
+import { AuthProvider } from "./Context/AuthContext.jsx";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/team" element={<Team />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/team" element={<Team />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
