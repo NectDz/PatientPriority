33fd5e1a7cf3a19178f1844a54214900f866eda5
NectDz <54990613+NectDz@users.noreply.github.com>
2024-09-23 20:04:56 -0400

    Added Navbar to all pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Team from "./pages/Team/Team";
 import { AuthProvider } from "./Context/AuthContext.jsx";
+import Layout from "./routes/Layout";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}></Route>
-            <Route path="/team" element={<Team />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />}></Route>
+              <Route path="/team" element={<Team />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </Layout>
       </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
