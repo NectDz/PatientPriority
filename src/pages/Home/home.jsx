1c11fdeb04310f1b7e069569f4144ebd959de57f
Kazi Anwar <kazisameen2702@gmail.com>
2024-09-25 14:39:12 -0400

    basic colors, heading & text added to home.jsx

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,15 +1,40 @@
-import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
+import React from 'react';
+import { ChakraProvider, Box, Heading, Text, Button } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
+  const navigate = useNavigate(); //from react router, use to redirect to pages
+
+  const handleDoctorLogin = () => { navigate('/doctorSignIn'); };
+
+  const handlePatientLogin = () => { navigate('/patient-login'); };
+
   return (
     <ChakraProvider>
       <Box
         display="flex"
+        flexDirection="column"
         justifyContent="center"
         alignItems="center"
-        minH="100vh"
+        height="100vh"
+        width="100vw"
+        backgroundColor="#EEF4ED" //we can change this
       >
-        <Heading>Hello World</Heading>
+
+        <Heading as="h1" size="2xl" color="#134074" mb="4">
+          Welcome to PatientPriority
+        </Heading>
+
+        <Text fontSize="xl" color="#13315C" mb="10" textAlign="center">
+          Helping patients and doctors stay connected, informed, and proactive.
+        </Text>
+        
+        <Box display="flex" gap="4">
+          
+          
+
+        </Box>
+        
       </Box>
     </ChakraProvider>
   );
