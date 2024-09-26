7136e395a97640daa5571d46edade61b854a0490
Kazi Anwar <kazisameen2702@gmail.com>
2024-09-26 19:01:33 -0400

    edited navbar to show PP w/ drop shadow per figma

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,23 +3,20 @@ import { ChakraProvider, Box, Flex, Link, Stack, Text } from "@chakra-ui/react";
 function Navbar() {
   return (
     <ChakraProvider>
-      <Box position="fixed" top="0" width="100%" zIndex="100" bg="transparent">
-        <Flex as="nav" justify="left" py={4} px={8}>
+      <Box position="fixed" top="0" width="100%" zIndex="100" bg="white" boxShadow="lg" p={4} borderRadius="md">
+        <Flex as="nav" justify="center" py={4} px={8}>
           <Stack direction="row" spacing={8} align="center">
             <Link
               href="/"
               _hover={{ textDecoration: "none", color: "teal.500" }}
             >
-              <Text fontSize="lg" fontWeight="bold">
+              <Text
+                fontSize="6xl"
+                fontWeight="bold"
+                color="#5AACA8"
+                sx={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)" }} //text shadow!!
+              >
                 PatientPriority
-              </Text>
-            </Link>
-            <Link
-              href="/team"
-              _hover={{ textDecoration: "none", color: "teal.500" }}
-            >
-              <Text fontSize="lg" fontWeight="bold">
-                Team
               </Text>
             </Link>
           </Stack>
