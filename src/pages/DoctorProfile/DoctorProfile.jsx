8f15f8646eb8b15143c512bb9a77f8b3a4fcaccf
Dee_ <ahmad.deedat91@myhunter.cuny.edu>
2024-10-16 21:23:23 -0400

    Test doc profile

---
+++ src/pages/DoctorProfile/DoctorProfile.jsx
@@ -0,0 +1,67 @@
+import { ChakraProvider, Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import SidePanel from "./sidePanel";
+import { useRef, useState } from "react";
+import ProfileCard from "./Cards/ProfileCard";
+import OverviewCard from "./Cards/OverviewCard";
+import CalendarCard from "./Cards/CalendarCard";
+import TranscriptionCard from "./Cards/TranscriptionCard";
+
+function PatientProfile() {
+  const profileRef = useRef(null);
+  const overviewRef = useRef(null);
+  const calendarRef = useRef(null);
+  const transcriptionRef = useRef(null);
+
+  const scrollToSection = (sectionId) => {
+    const refMap = {
+      "profile-card": profileRef,
+      "overview-card": overviewRef,
+      "calendar-card": calendarRef,
+      "transcription-card": transcriptionRef,
+    };
+    if (refMap[sectionId]?.current) {
+      refMap[sectionId].current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <ChakraProvider>
+      <Flex pt="50px" minH="100vh" bg="#EFF8F8">
+        <SidePanel scrollToSection={scrollToSection} />
+
+        {/* Main Content Area */}
+        <Grid
+          pl="8"
+          pt="60px"
+          marginLeft="220px"  // Set fixed margin to account for side panel width
+          templateRows="repeat(8, 1fr)"
+          templateColumns="repeat(1, 1fr)"  // Only one column for full-width cards
+          gap={6}  // Increased gap for better spacing
+          width="100%"  // Set the grid to use full width of the container
+        >
+          {/* Profile Card */}
+          <GridItem ref={profileRef} id="profile-card">
+            <ProfileCard />
+          </GridItem>
+
+          {/* Overview Card */}
+          <GridItem ref={overviewRef} id="overview-card">
+            <OverviewCard />
+          </GridItem>
+
+          {/* Reminders / Appointments (CalendarCard) */}
+          <GridItem ref={calendarRef} id="calendar-card">
+            <CalendarCard />
+          </GridItem>
+
+          {/* Transcription / Summary (TranscriptionCard) */}
+          <GridItem ref={transcriptionRef} id="transcription-card">
+            <TranscriptionCard />
+          </GridItem>
+        </Grid>
+      </Flex>
+    </ChakraProvider>
+  );
+}
+
+export default PatientProfile;
