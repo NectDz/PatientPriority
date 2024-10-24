9c3f331e241e613ffbe5224fc1a205bcc5bc2c82
Lubna <lubna.asha.1125@gmail.com>
2024-10-24 03:28:57 -0400

    temporary Recent Patients layout on Doctor Home Page using Chakra table component

--- src/pages/DoctorProfile/doctorHomePage.jsx
+++ src/pages/DoctorProfile/doctorHomePage.jsx
@@ -10,23 +10,31 @@ import {
   Avatar,
   VStack,
   HStack,
+  Divider,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react";
 import { CalendarIcon } from "@chakra-ui/icons";
 import { FaUserFriends, FaUserMd } from "react-icons/fa";
 
 //Team Images
-import Deedat from "../../assets/Team/Deedat.png"
-import Abir from "../../assets/Team/Abir.png"
-import Rahat from "../../assets/Team/Rahat.png"
-import Kazi from "../../assets/Team/Kazi.png"
-import Kevin from "../../assets/Team/Kevin.png"
-import Lubna from "../../assets/Team/Lubna.png"
-
+import Deedat from "../../assets/Team/Deedat.png";
+import Abir from "../../assets/Team/Abir.png";
+import Rahat from "../../assets/Team/Rahat.png";
+import Kazi from "../../assets/Team/Kazi.png";
+import Kevin from "../../assets/Team/Kevin.png";
+import Lubna from "../../assets/Team/Lubna.png";
 
 function DoctorHome() {
   return (
     <ChakraProvider>
-      <Card borderRadius="20px" height="200px" width="100%">
+      <Card borderRadius="20px" height="100%" width="100%">
         <CardHeader bg="#EFF8F8" borderRadius="20px 20px 0px 0px">
           <Heading fontSize="2xl" color="black">
             <Icon as={CalendarIcon} mr={2} />
@@ -45,10 +53,11 @@ function DoctorHome() {
         </CardBody>
       </Card>
 
+      {/* RECENT PATIENTS CARD */}
       <Card
         // bg="#EFF8F8"
         borderRadius="20px"
-        height="200px"
+        height="100%"
         width="100%"
       >
         <CardHeader bg="#EFF8F8" borderRadius="20px 20px 0px 0px">
@@ -60,19 +69,49 @@ function DoctorHome() {
         <CardBody
           display="flex"
           flexDirection="column"
-          justifyContent="center"
+          //   justifyContent="center"
           borderRadius="0px 0px 20px 20px"
         >
           <Text fontSize="lg" color="#737373">
             See your most recent patient interactions.
           </Text>
+        {/* Display patients using table component*/}
+          <TableContainer mt={5}>
+            <Table variant="striped"> {/* adds the color between rows */}
+
+              <Thead> {/* this is the table head aka categories */}
+                <Tr> 
+                  <Th>Image</Th>
+                  <Th>Name</Th>
+                  <Th>Date of Visit</Th>
+                  <Th>Appointment Type</Th>
+                </Tr>
+              </Thead>
+
+              <Tbody>
+                <Tr>
+                  <Td>
+                    <Avatar
+                      size="xl"
+                      name="Kazi"
+                      src="https://www.usatoday.com/gcdn/presto/2020/08/28/USAT/efafa6ba-5d7c-4218-8684-37436e378850-liam_payne.JPG?crop=2978,1676,x0,y447"
+                    />
+                  </Td>
+                  <Td>Liam Payne</Td>
+                  <Td>October 16, 2024</Td>
+                  <Td>Drug Test</Td>
+                </Tr>
+                
+              </Tbody>
+
+            </Table>
+            <Divider />
+          </TableContainer>
         </CardBody>
       </Card>
 
-
-
-    {/* DOCTOR TEAM CARD */}
-      <Card borderRadius="20px" height="300px" width="100%">
+      {/* DOCTOR TEAM CARD */}
+      <Card borderRadius="20px" height="100%" width="100%">
         <CardHeader bg="#EFF8F8" borderRadius="20px 20px 0px 0px">
           <Heading fontSize="2xl" color="black">
             <Icon as={FaUserFriends} mr={2} />
@@ -84,64 +123,42 @@ function DoctorHome() {
           flexDirection="column"
           justifyContent="center"
           borderRadius="0px 0px 20px 20px"
+          overflowY="auto"
+          padding={4}
         >
           <HStack align="center" justify="center" spacing={20}>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. A Bear"
-                src={Abir} // random image
-              />
+              <Avatar size="2xl" name="Dr. A Bear" src={Abir} />
               <Text fontSize="lg" color="#737373">
                 Dr. A Bear
               </Text>
             </VStack>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. Kave"
-                src={Kevin} // random image
-              />
+              <Avatar size="2xl" name="Dr. Kave" src={Kevin} />
               <Text fontSize="lg" color="#737373">
                 Dr. Kave
               </Text>
             </VStack>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. Rahhh"
-                src={Rahat} // random image
-              />
+              <Avatar size="2xl" name="Dr. Rahhh" src={Rahat} />
               <Text fontSize="lg" color="#737373">
                 Dr. Rahhh
               </Text>
             </VStack>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. Lube"
-                src={Lubna} // random image
-              />
+              <Avatar size="2xl" name="Dr. Lube" src={Lubna} />
               <Text fontSize="lg" color="#737373">
                 Dr. Lube
               </Text>
             </VStack>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. Deed"
-                src={Deedat} // random image
-              />
+              <Avatar size="2xl" name="Dr. Deed" src={Deedat} />
               <Text fontSize="lg" color="#737373">
                 Dr. Deed
               </Text>
             </VStack>
             <VStack>
-              <Avatar
-                size="2xl"
-                name="Dr. Kazoo"
-                src={Kazi} // random image
-              />
+              <Avatar size="2xl" name="Dr. Kazoo" src={Kazi} />
               <Text fontSize="lg" color="#737373">
                 Dr. Kazoo
               </Text>
