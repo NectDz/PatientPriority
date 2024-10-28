015e43c82de74c0591a5be7d528cde6876c46759
Kazi Anwar <kazisameen2702@gmail.com>
2024-10-28 00:10:17 -0400

    added email with verification code in handle submit function

--- src/pages/Patients/AddPatient.jsx
+++ src/pages/Patients/AddPatient.jsx
@@ -155,7 +155,17 @@ function AddPatient() {
       await addDoc(collection(db, "patient_codes"), patientCodeData);
       console.log("Patient Code Added:", patientCodeData);
 
-      
+      //send email with verification code
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          recipient_email: formData.email,
+          firstName: formData.firstName,
+          verificationCode: code,
+        },
+        import.meta.env.VITE_EMAILJS_USER_ID
+      );
 
       setSuccessMessage(
         `Patient added! Verification Code: ${code}. Code has been generated and emailed to patient -- please reach out to patient & ask them to sign up for patient priority access.`
